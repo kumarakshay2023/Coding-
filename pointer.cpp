05aea24c190534539a5136e1b4dd253b552269e4
Akshay kumar <kumarakshay2023@gmail.com>
2022-02-21 21:43:37 +0530

    new commit

---
+++ pointer.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int num=5;
+    int *p=&num;
+    /*
+    // int *q=0;
+    // q=&num;  both *p and *q are same 
+    // cout<<p<<endl;
+    // cout<<&num; // same addrees will print for both 
+    // cout<<num<<endl;
+    // cout<<*p; // derefrence operator uss adrress pr jo value hai usko print kr do
+    cout<<sizeof(p)<<endl;
+    cout<<sizeof(num);
+    int *ptr// pointer to int is created and pointed to some garbage address
+    // never do this bad practice
+    */
+   /*
+     coyping a pointer
+    int *q=p;
+    cout<<p<<" - "<<q<<endl;
+    cout<<*p<<" - "<<*q;
+    */
+}
